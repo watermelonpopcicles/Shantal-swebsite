21725fb724480fcdecc615848370936aad08c98f
WATERMELONPOP <xxtobynxx@gmail.com>
2021-10-04 16:58:59 -0700

    updated code

--- app/Shantal's Cows and Bulls game.py
+++ app/Shantal's Cows and Bulls game.py
@@ -1,5 +1,47 @@
 import random
 from tkinter import *
+
+letters =["A", "B", "C", "D", "E"]
+win = False
+
+key = ""
+for i in range (4):
+    key += random.choice(letters)
+#    print(key)
+letterdic = dict(zip(list(key),[0]*len(key)))
+
+bull = 0
+cow = 0
+
+def pickChoice():
+    global bull, cow,letterdic
+    
+    guess = default.get() + default2.get() + default3.get() + default4.get()
+
+    if guess == key:
+        win = True
+        print ("You Win!!!")
+        
+    else:
+        for i in range(len(guess)):
+            if(guess[i] not in key):
+                continue
+            
+            if (guess[i]==key[i]):
+                bull = bull + 1
+                letterdic[key[i]] += 1
+            
+            if (letterdic[guess[i]] < guess.count(guess[i])):
+                cow = cow + 1
+
+    l = Label(scrollable_frame, text = "Bull = " +str(bull) + ", " +"Cow = " + str(cow) + "  " +guess, bg = "beige", font =("Didact Gothic", 12))
+    l.pack()
+    
+    bull = 0
+    cow = 0
+    letterdic = dict(zip(list(key),[0]*len(key)))
+
+
 window = Tk()
 window.geometry("2000x1000")
 window.configure(bg = "beige")
@@ -48,7 +90,7 @@ dropdown4.pack(side = LEFT)
 frame1.pack()
 
 #submit button
-submitbutton = Button(window,text = "Submit", width=15, height = 3, font = ("Didact Gothic", 16))
+submitbutton = Button(window,text = "Submit", width=15, height = 3, font = ("Didact Gothic", 16),command = pickChoice)
 submitbutton.config(bg = "black",fg = "white")
 submitbutton.config(bg = "black",fg = "white")
 
@@ -70,11 +112,8 @@ scrollable_frame.bind(
 canvas.create_window((0,0), window = scrollable_frame, anchor = "nw")
 
 canvas.configure(yscrollcommand = scrollbar.set)
-for i in range (20):
-    l = Label(scrollable_frame, text = " cow = 7 bull = 9", bg = "beige",
-              font =("Didact Gothic", 12))
 
-    l.pack()
+    
 container.pack()
 canvas.pack()
 canvas.pack(side = "left", fill = "both", expand = True)
@@ -87,45 +126,12 @@ scrollbar.pack(side = "right", fill = "y")
 window.mainloop()
 
 #game
-letters =["A", "B", "C", "D", "E"]
-win = False
+
 
 ##print ("""Welcome to my Cows and Bulls game. I will have a 4 digit combonation using the letters A, B, C, D, and E. Then you will guess the combonation.
 ##Every letter that is in the correct combonation counts as a Cow.  A letter that is correct AND in the right place is a Bull. Using this information,
 ##try to get the right combonation in 20 guesses. """)
 
-key = ""
-for i in range (4):
-    key += random.choice(letters)
-#    print(key)
-letterdic = dict(zip(list(key),[0]*len(key)))
-
-bull = 0
-cow = 0
-
-
-for i in range(20):
-    guess = input("What is you first guess?")
-    if guess == key:
-        win = True
-        print ("You Win!!!")
-        break
-    else:
-        for i in range(len(guess)):
-            if(guess[i] not in key):
-                continue
-            
-            if (guess[i]==key[i]):
-                bull = bull + 1
-                letterdic[key[i]] += 1
-            
-            if (letterdic[guess[i]] < guess.count(guess[i])):
-                cow = cow + 1
-    print("bull = " +str(bull))
-    print("cow = " + str(cow))
-    bull = 0
-    cow = 0
-    letterdic = dict(zip(list(key),[0]*len(key)))
 
 
 
